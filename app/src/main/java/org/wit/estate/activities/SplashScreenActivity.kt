c81afe2d9f8f8d984bceda1c4f27b7f2e7b2a214
Niall Crowe <20089334@mail.wit.ie>
2022-12-13 14:52:07 +0000

    Added a splash screen that displays when the app is launched
    Signed-off-by: Niall Crowe <20089334@mail.wit.ie>

---
+++ app/src/main/java/org/wit/estate/activities/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package org.wit.estate.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import org.wit.estate.R
+
+class SplashScreenActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_splash_screen)
+
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, EstateListActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+        }
+    }
